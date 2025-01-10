be248beb287b5c35d5c6fe6dd3d2efcf03979d21
Aditya Jain <adityajainghetal@gmail.com>
2025-01-10 15:23:01 +0530

    Error_handling

--- Error_handling/server/app.js
+++ Error_handling/server/app.js
@@ -8,11 +8,21 @@ app.use(cors());
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json())
 
-mongoose.connect("mongodb+srv://adityajainghetal:<kyn3m1kgXutowhoz>@cluster0.idrso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+mongoose.connect("mongodb://127.0.0.1:27017/jwtAditya").then(()=>{
   console.log("DB connected!!!");
 })
 
-app.use("/books", userRouter);
+app.use("/",(req,res,next)=>{
+    throw new Error ("Something went wrong");
+    next();
+})
+
+app.use((err,req,res,next)=>{
+  res.status(500).json({error: err.message})
+})
+
+
+app.use("/user", userRouter);
 
 app.listen(8000, ()=>{
     console.log("server run on 8000 Port!");
